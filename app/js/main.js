695f257f88528353f22db6e46a6bd744365b7521
Alexander Savka <savka.alexandr@gmail.com>
2016-02-09 08:08:00 +0700

    Добавлен PopUp

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 	console.log("Я на главной странице");
 });
+
+function getName (str){
+    if (str.lastIndexOf('\\')){
+        var i = str.lastIndexOf('\\')+1;
+    }
+    else{
+        var i = str.lastIndexOf('/')+1;
+    }						
+    var filename = str.slice(i);			
+    var uploaded = document.getElementById("open-button-placeholder");
+    uploaded.innerHTML = filename;
+}
